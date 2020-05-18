6a1d9ba974ca47c0e9b9c7f4a63b7cd51911605f
grebnev-av <grebnev.av@gmail.com>
2020-05-18 11:35:05 +0300

    [195538] fix intergations

--- src/editor/components/BuilderSiteSettingsIntegrations.vue
+++ src/editor/components/BuilderSiteSettingsIntegrations.vue
@@ -7,6 +7,7 @@
             v-for="integration in integrations"
             :key="integration.name"
             @click="selectIntegration(integration.name)"
+            v-if="integration.name !== 'mailchimp' || (isGuest === false && integration.name === 'mailchimp')"
           >
             <div class="b-integrations__item-icon"
               :style="{'background-image': 'url(' + integration.icon + ')'}"
@@ -70,6 +71,10 @@ export default {
 
     isIntegrationVisible () {
       return last(this.$route.path.split('/')) !== 'integrations'
+    },
+
+    isGuest () {
+      return localStorage.getItem('guest') !== null
     }
   },
 
